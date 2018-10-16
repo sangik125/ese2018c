b4473281a9050b3773dffe3d91351ad26649d028
sangik125 <sangik125@naver.com>
2018-10-16 16:44:44 +0900

    add func2 function

---
+++ func2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "func2.h"
+
+void foo2()
+{
+  printf("foo2\n");
+}
--- myapp.c
+++ myapp.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "func1.h"
-
+#include "func2.h"
 int main()
 {
   foo1();
+  foo2();
   return 0;
 }
