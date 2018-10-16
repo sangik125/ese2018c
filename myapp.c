8de3cda2ea07b58edfc24fea7c08ffb7a862891c
sangik125 <sangik125@naver.com>
2018-10-16 16:56:53 +0900

    added turbo

--- myapp.c
+++ myapp.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include "func1.h"
 #include "func2.h"
+
+void turbo_on()
+{
+  printf("turbo on\n");
+}
+
+void turbo_off()
+{
+  printf("turbo off\n");
+}
+
+
+
+
+
 int main()
 {
+
+  turbo_on();
   foo1();
   foo2();
+  turbo_off();
+
+
   return 0;
 }
